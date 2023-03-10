1748238c6dcdb61d2e46c69063b88fd7dc6f0765
PedroMiceli <pedro.miceli@hotmail.com>
2023-03-10 17:38:21 -0300

    area de cadastro continuada

--- healthcheck/login/views.py
+++ healthcheck/login/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
     else:
-        us
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = User.objects.get(username=username)
 
 def login(request):
 
